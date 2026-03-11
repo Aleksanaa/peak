a71cfd2228715ec3353ddb083648da8aeb4307c9
aleksana <me@aleksana.moe>
2026-03-12 00:51:28 +0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Acme-inspired TUI Text Editor";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          packages = rec {
+            peak =
+              with pkgs;
+              buildGoModule {
+                name = "peak";
+
+                src = lib.cleanSource ./.;
+
+                vendorHash = "sha256-MO5zFLK9Q9htLPmCF7paikqpJD4WgWbVzdMecOxrhNo=";
+
+                env.CGO_ENABLED = 0;
+
+                ldflags = [
+                  "-s"
+                  "-w"
+                ];
+
+                doCheck = false; # breaks regexp, test data missing
+              };
+            default = peak;
+          };
+
+          overlayAttrs = {
+            inherit (config.packages) peak;
+          };
+        };
+    };
+}
