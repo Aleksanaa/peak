d8f310e2055d89de4e9abcc9ae980063e8521bc1
Moraxyc <i@qaq.li>
2026-05-27 01:45:42 +0800

    use nix-systems/default to generate systems

--- flake.nix
+++ flake.nix
@@ -4,15 +4,13 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+      systems = import inputs.systems;
 
       imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
 
